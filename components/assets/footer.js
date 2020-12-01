a6246dcddd6606bb6307e58a3c0e51c850a09a27
Alf <57676676+alfredsaveron@users.noreply.github.com>
2020-12-01 22:12:15 +0300

    Update footer.js

--- components/assets/footer.js
+++ components/assets/footer.js
@@ -9,19 +9,23 @@ export default function Footer() {
                     <p className="leading-relaxed text-base">
                         Most of the components are taken from GitHub and other template websites. What is Kaguwo Network? Kaguwo Network is a group jointly managed by a group of developers and developing open source projects. So where do Kaguwo Network Developers live? In fact, we all live in Turkey. But we are all in different cities, 2 of our members live in Adana, 1 member lives in Izmir, and the other 1 member lives in Muğla.
                     </p>
-                    <a className="text-purple-500 inline-flex items-center mt-4" href="https://github.com/kaguwomin/modern-next-portfolio"> GitHub Repository
+                    <a className="text-purple-500 inline-flex items-center mt-4" href="https://github.com/KaguwoNetwork"> Kaguwo Network
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>
                     </a>
                 </div>
                 <div className="flex flex-col md:w-1/2 md:pl-12">
-                    <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">ASSETS FROM OTHER CREATORS</h2>
+                    <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">ASSETS AND ADDITIONAL LINKS FROM OTHER CREATORS</h2>
                     <nav className="flex flex-wrap list-none -mb-1">
                         <li className="lg:w-1/3 mb-1 w-1/2">
                             <a className="hover:text-white" href="https://github.com/BraydenTW/react-tailwind-portfolio/">Brayden W. Card Design</a>
                         </li>
                         <hr />
+                        <li className="lg:w-1/2 mb-1 w-1/3">
+                            <a className="hover:text-white" href="https://alfreddo.ga">Portfolio & Main Portfolio and Photos!</a>
+                        </li>
+                         <hr />
                         <li className="lg:w-1/2 mb-1 w-1/3">
                             <a className="hover:text-white" href="https://mertjf.github.io/tailblocks/">Skills & Footer Design</a>
                         </li>
@@ -29,7 +33,7 @@ export default function Footer() {
                     <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3 mt-12">CONTACT</h2>
                     <nav className="flex flex-wrap list-none -mb-1">
                         <li className="lg:w-1/3 mb-1 w-1/2">
-                            <a className="hover:text-white" href="https://t.me/kaguwo">
+                            <a className="hover:text-white" href="https://twitter.com/alfredsaveron">
                                 <FaTelegram className="ml-1 w-12 h-12 sm:w-10 sm:h-10" />
                             </a>
                         </li>
