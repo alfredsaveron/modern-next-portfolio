b540bf89d945c72263b8a39c299dff4473ea5eb7
Alf <57676676+alfredsaveron@users.noreply.github.com>
2020-12-01 21:15:35 +0300

    Update card.js

--- components/assets/card.js
+++ components/assets/card.js
@@ -13,17 +13,17 @@ export default function Card() {
                 </div>
                 <div className="text-center mt-5">
                     <p className="text-lg sm:text-xl font-semibold text-gray-900 dark:text-gray-50">
-                        Hüseyin "Kaguwo" Yalçın
+                        Alfred Saveron
                     </p>
                     <p className="text-xs sm:text-base text-gray-400 pt-2 pb-4 px-5 w-58 inline-block border-b-2">
-                        Front-End Developer
+                        Front-End Web Developer
                     </p>
                     <div className="flex align-center justify-center mt-4">
-                        <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300 dark:text-gray-200 dark:hover:bg-gray-500" href="https://github.com/kaguwomin">
+                        <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300 dark:text-gray-200 dark:hover:bg-gray-500" href="https://github.com/alfredsaveron">
                             <FaGithub />
                             <span className="sr-only">Github</span>
                         </a>
-                        <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-500 hover:bg-blue-500 rounded-full hover:text-white transition-colors duration-300" href="https://twitter.com/kaguwomin">
+                        <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-500 hover:bg-blue-500 rounded-full hover:text-white transition-colors duration-300" href="https://twitter.com/alfredsaveron">
                             <FaTwitter />
                             <span className="sr-only">Twitter</span>
                         </a>
@@ -31,7 +31,7 @@ export default function Card() {
                             <FaDiscord />
                             <span className="sr-only">Discord</span>
                         </a>
-                        <a className="text-xl mt-1 p-1 sm:m-2 sm:p-2 text-indigo-500 hover:bg-indigo-500 rounded-full hover:text-white transition-colors duration-300" href="mailto:business@kaguwo.com">
+                        <a className="text-xl mt-1 p-1 sm:m-2 sm:p-2 text-indigo-500 hover:bg-indigo-500 rounded-full hover:text-white transition-colors duration-300" href="https://twitter.com/alfredsaveron">
                             <FaRegPaperPlane />
                             <span className="sr-only">Email</span>
                         </a>
