8818cdb4b0c9f9d004583121af20d2debc3e2294
Alf <57676676+alfredsaveron@users.noreply.github.com>
2020-12-02 08:46:02 +0300

    Update experience.js

--- components/assets/experience.js
+++ components/assets/experience.js
@@ -18,7 +18,7 @@ export default function Skills() {
                 <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
                     <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col text-center items-center">
                         <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-800 text-blue-400 mb-5 flex-shrink-0 dark:bg-gray-200">
-                            <FaReact className="text-5xl mx-auto inline-block" />
+                            <FaHtml5 className="text-5xl mx-auto inline-block" />
                         </div>
                         <div className="flex-grow">
                             <h2 className="text-black text-lg title-font font-medium mb-3 dark:text-white">HTML</h2>
